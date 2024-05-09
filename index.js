b2b2a3858599850d22272dfaeb32d65373412860
Daniil Savchik <savchik.official@gmail.com>
2024-05-09 13:54:57 +0300

    fix: fix index.js

--- index.js
+++ index.js
@@ -41,6 +41,8 @@ app.use(express.json());
 app.use(cors());
 app.use('/uploads', express.static('uploads'));
 
+
+app.get("/", (req, res) => res.send("Express on Vercel"));
 app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login);
 app.post('/auth/register', registerValidation, handleValidationErrors, UserController.register);
 app.get('/auth/me', checkAuth, UserController.getMe);
@@ -66,7 +68,7 @@ app.get('/comments/:postId', CommentController.getCommentsByPostId)
 
 
 
-app.listen(process.env.PORT || 4444, (err) => {
+app.listen(3000, (err) => {
     if (err) {
         return console.log(err);
     }
